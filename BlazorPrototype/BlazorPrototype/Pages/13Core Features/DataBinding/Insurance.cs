5bf781fb962baca41a62b900015d1eedd1e45737
Naveen <naveenat@m-s.in>
2020-02-25 11:46:42 +0530

    ADDED: Templated Components

---
+++ BlazorPrototype/BlazorPrototype/Pages/13Core Features/DataBinding/Insurance.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPrototype
+{
+    public class Insurance
+    {
+        #region Properties
+        public string ShortCaption { get; set; }
+        public string Caption { get; set; }
+        #endregion
+    }
+}
